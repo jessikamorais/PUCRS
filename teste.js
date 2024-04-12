d8a300f92612b64bf6e90165fb90ba450ac5be75
Jessika Morais <jessikanmorais@gmail.com>
2024-04-12 13:27:50 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,15 @@
+const date = new Date;
+const curretYear=date.getFullYear();
+
+
+var pessoa0 = {
+	nome: ['Fulano', 'de Tal'],
+	anoDeNascimento: 1990,
+	profissao: 'Estudante',
+	calculaIdade: function() {
+		return newDate().getFullYear() - this.anoDeNascimento;
+	
+	},
+}; 
+
+console.log (pessoa0)
